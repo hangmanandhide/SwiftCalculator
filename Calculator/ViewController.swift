33e29385facfa4cb01da0e7910b6a222cff1868f
HangmanAndHide <9014584+hangmanandhide@users.noreply.github.com>
2020-03-09 21:43:15 -0700

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mehdi Ghannadan on 3/9/20.
+//  Copyright © 2020 Mehdi Ghannadan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
